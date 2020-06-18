e990370822a71565fdb20776a4c67602087a3b67
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-06-19 01:23:06 +0200

    x

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,87 @@
-import React from 'react';
-import {NavLink} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
+  const [toggled, setToggled] = useState(false);
+
+  let nav = null;
+  let btn = null;
+
+  useEffect(() => {
+    nav = document.getElementsByClassName("nav")[0];
+    btn = document.getElementsByClassName("nav-tgl")[0];
+    console.log(nav);
+  });
+
+  const toggler = (e) => {
+    if (!toggled) {
+      setToggled(true);
+      e.target.classList.add("toggled");
+      nav.classList.add("active");
+      setTimeout(() => {
+        nav.classList.add("display-block");
+      }, 500)
+    } else {
+      setToggled(false);
+      e.target.classList.remove("toggled");
+      nav.classList.remove("active", "display-block");
+    }
+  };
+
   return (
     <div className="header">
       <div className="header-left">
-      <img src={require("../img/logo.png")} className="logo"/>
-      <img src={require("../img/light-green-bully.png")} className="logo-bully"/>
+        <img src={require("../img/logo.png")} className="logo" />
+        <img
+          src={require("../img/light-green-bully.png")}
+          className="logo-bully"
+        />
       </div>
       <div className="header-right">
-      <nav>
-        <NavLink to="/"><li>home</li></NavLink>
-        <NavLink to="/angebot"><li>angebot</li></NavLink>
-        <NavLink to="/ueber-mich"><li>über mich</li></NavLink>
-        <NavLink to="/referenzen"><li>referenzen</li></NavLink>
-        <NavLink to="/kontakt"><li>kontakt</li></NavLink>
-      </nav>
+        <nav className="nav-desktop">
+          <NavLink to="/">
+            <li>home</li>
+          </NavLink>
+          <NavLink to="/angebot">
+            <li>angebot</li>
+          </NavLink>
+          <NavLink to="/ueber-mich">
+            <li>über mich</li>
+          </NavLink>
+          <NavLink to="/referenzen">
+            <li>referenzen</li>
+          </NavLink>
+          <NavLink to="/kontakt">
+            <li>kontakt</li>
+          </NavLink>
+        </nav>
+        <button
+          className="nav-tgl"
+          type="button"
+          aria-label="toggle menu"
+          onClick={toggler}
+        >
+          <span aria-hidden="true"></span>
+        </button>
+        <nav className="nav">
+          <ul>
+            <NavLink to="/">
+              <li onClick="toggler">home</li>
+            </NavLink>
+            <NavLink to="/angebot">
+              <li onClick="toggler">angebot</li>
+            </NavLink>
+            <NavLink to="/ueber-mich">
+              <li onClick="toggler">über mich</li>
+            </NavLink>
+            <NavLink to="/referenzen">
+              <li onClick="toggler">referenzen</li>
+            </NavLink>
+            <NavLink to="/kontakt">
+              <li onClick="toggler">kontakt</li>
+            </NavLink>
+          </ul>
+        </nav>
       </div>
     </div>
   );
